0cf5cb971c35a80d831993ddfe034bbaf8d920c7
Frankie Dumas <frankiedumas@icloud.com>
2020-11-25 17:23:52 +0200

    Initial Commit

---
+++ Solaris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Solaris
+//
+//  Created by Frankie Dumas on 2020/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
